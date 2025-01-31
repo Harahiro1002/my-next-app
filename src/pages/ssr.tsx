339471a37a714af901c7046508dcbf0f6e4585dd
Harahiro <hiroshi.harasawa.1002@gmail.com>
2025-02-01 02:14:57 +0900

    Add SSR and SSG pages

---
+++ src/pages/ssr.tsx
@@ -0,0 +1,26 @@
+import texts from "@/../public/locales/ja.json";
+import { Post } from "@/types/post";
+
+type Props = {
+    data: Post;
+  };
+
+export async function getServerSideProps() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+    const data: Post = await res.json();
+
+    return { props: { data } };
+}
+
+export default function SSRPage({ data }: Props){
+    return (
+        <div>
+            <h1>
+                {texts.ssr.title}
+            </h1>
+            <p>
+                {data.title}
+            </p>
+        </div>
+    );
+}
